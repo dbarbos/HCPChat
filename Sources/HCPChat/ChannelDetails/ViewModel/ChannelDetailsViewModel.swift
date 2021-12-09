c3562bd8f1b66c359c8821168aa687f1e03607a7
Diler Barbosa <tqi_dbarbosa@uolinc.com>
2021-12-09 17:48:50 -0300

    created channel details viewmodel

---
+++ Sources/HCPChat/ChannelDetails/ViewModel/ChannelDetailsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelDetailsViewModel.swift
+//  
+//
+//  Created by Dilermando Barbosa Junior on 09/12/21.
+//
+
+import Foundation
+
+
+class ChannelDetailsViewModel {
+  
+  var users: [ChannelDetailsUser] = [
+    ChannelDetailsUser(avatarUrl: "", name: "Dilermando Barbosa", roles: [ChannelDetailsUserRole(value: "Admin")]),
+    ChannelDetailsUser(avatarUrl: "", name: "Uke Jain", roles: [ChannelDetailsUserRole(value: "Field Tech")])
+  ]
+  
+}
