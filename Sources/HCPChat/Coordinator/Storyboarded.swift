9b848007270776fa48fe5665f77942cefb9d1950
Diler Barbosa <tqi_dbarbosa@uolinc.com>
2021-12-08 21:31:55 -0300

    bugfix

--- Sources/HCPChat/Coordinator/Storyboarded.swift
+++ Sources/HCPChat/Coordinator/Storyboarded.swift
@@ -15,14 +15,8 @@ protocol Storyboarded {
 extension Storyboarded where Self: UIViewController {
   static func instantiate() -> Self {
     let id = String(describing: self)
-    let storyboard = UIStoryboard(name: id, bundle: bundle)
+    let storyboard = UIStoryboard(name: id, bundle: HCPChat.bundle)
     
     return storyboard.instantiateViewController(withIdentifier: id) as! Self
   }
-  
-  private static let bundle: Bundle? = {
-      let bundle = Bundle.main
-      let path = "\(bundle.bundlePath)/\(featureIdentifier).bundle"
-      return Bundle(path: path)
-  }()
 }
--- Sources/HCPChat/HCPChat.swift
+++ Sources/HCPChat/HCPChat.swift
@@ -1,9 +1,16 @@
 import UIKit
 
 public struct HCPChat {
+  static let featureIdentifier = "HCPChat"
 
   static public func start(flow: CoordinatorFlow = .channelDetails, from navigationController: UINavigationController) {
     let mainCoordinator = MainCoordinator(navigationController)
     mainCoordinator.start()
   }
+  
+  public static let bundle: Bundle? = {
+      let bundle = Bundle.main
+      let path = "\(bundle.bundlePath)/\(featureIdentifier).bundle"
+      return Bundle(path: path)
+  }()
 }
